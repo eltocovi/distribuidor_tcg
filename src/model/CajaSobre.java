148f820689e000b97596e008c13ca2ebffc89cd4
eltocovi <victormanuel1998@gmail.com>
2025-12-01 20:43:42 -0300

    feat/main: Producto corregido, cajaSobre creado

--- src/model/CajaSobre.java
+++ src/model/CajaSobre.java
@@ -5,5 +5,33 @@ package model;
  * @author Vitoco
  */
 public class CajaSobre extends Producto {
+    private int cantidadPorCaja;
+
+    public CajaSobre() {
+    }
+
+    public CajaSobre(int cantidadPorCaja, String edicion, String nombre, String linea, String tipo, int stock, int precio, String sku, String fecha_salida, String descripcion) {
+        super(edicion, nombre, linea, tipo, stock, precio, sku, fecha_salida, descripcion);
+        this.cantidadPorCaja = cantidadPorCaja;
+    }
+
+
+
+    public int getCantidadPorCaja() {
+        return cantidadPorCaja;
+    }
+
+    public void setCantidadPorCaja(int cantidadPorCaja) {
+        this.cantidadPorCaja = cantidadPorCaja;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+ "\n Cantidad Por Caja: " + cantidadPorCaja;
+    }
+
+    
+   
+    
     
 }
--- src/model/Producto.java
+++ src/model/Producto.java
@@ -1,22 +1,29 @@
-
+package model;
 /**
  *
  * @author Vitoco
  */
 public abstract class Producto {
     private String edicion, nombre, linea;
-    private int stock;
+    private String tipo;
+    private int stock, precio;
+    private String sku;
     private String fecha_salida;
+    private String descripcion;
 
     public Producto() {
     }
 
-    public Producto(String edicion, String nombre, String linea, int stock, String fecha_salida) {
+    public Producto(String edicion, String nombre, String linea, String tipo, int stock, int precio, String sku, String fecha_salida, String descripcion) {
         this.edicion = edicion;
         this.nombre = nombre;
         this.linea = linea;
+        this.tipo = tipo;
         this.stock = stock;
+        this.precio = precio;
+        this.sku = sku;
         this.fecha_salida = fecha_salida;
+        this.descripcion = descripcion;
     }
 
     public String getEdicion() {
@@ -43,12 +50,36 @@ public abstract class Producto {
         this.linea = linea;
     }
 
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
+
     public int getStock() {
         return stock;
     }
 
     public void setStock(int stock) {
         this.stock = stock;
+    }
+
+    public int getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(int precio) {
+        this.precio = precio;
+    }
+
+    public String getSku() {
+        return sku;
+    }
+
+    public void setSku(String sku) {
+        this.sku = sku;
     }
 
     public String getFecha_salida() {
@@ -59,13 +90,24 @@ public abstract class Producto {
         this.fecha_salida = fecha_salida;
     }
 
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+    
+
     
     
     
   
     @Override
     public String toString() {
-        return "Edicion: " + edicion + "\n Nombre: " + nombre + "\n Linea: " + linea + "\n Stock: " + stock + "\n Fecha Salida: " + fecha_salida ;
+        return "Edicion: " + edicion + "\n Nombre: " + nombre + "\n Linea: " + linea +"\n Tipo de producto: "+ tipo + "\n Stock: " + stock +"\n SKU: "+ sku +"\n Precio "+ precio+ "\n Fecha Salida: " + fecha_salida + "\n Descripcion: "+descripcion;
     }
     
     
