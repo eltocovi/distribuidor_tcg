a400d565ba72cac51fc3a6854aae1c4e8b044719
eltocovi <victormanuel1998@gmail.com>
2025-12-01 21:42:01 -0300

    feat/main: CONEXION DAO

---
+++ src/controller/ConexionDAO.java
@@ -0,0 +1,29 @@
+package controller;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+/**
+ *
+ * @author Vitoco
+ */
+public class ConexionDAO {
+    private static Connection conn;
+    String url = "jdbc:mysql://localhost:3306/distribuidora_tcg";
+    String usuario = "admin_tcg";
+    String password = "admin_tcg";
+
+    private ConexionDAO() throws SQLException {
+        DriverManager.registerDriver (new com.mysql.cj.jdbc.Driver());
+        conn = DriverManager.getConnection(url, usuario, password);
+    }
+
+    public static Connection getConnection() throws SQLException 
+    
+    {
+        if (conn == null) {
+            new ConexionDAO();
+        }
+        return conn;
+    }
+}
