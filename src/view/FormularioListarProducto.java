ffb84c48301f9f799b9b4a948e6268b2c13f7004
eltocovi <victormanuel1998@gmail.com>
2025-12-03 00:12:02 -0300

    feat/main: modificaciones ProductoDAO, añade listar Añade interfaz grafica listar

--- src/view/FormularioListarProducto.java
+++ src/view/FormularioListarProducto.java
@@ -68,13 +68,13 @@ public class FormularioListarProducto extends javax.swing.JFrame {
 
         jtListar.setModel(new javax.swing.table.DefaultTableModel(
             new Object [][] {
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null},
-                {null, null, null, null}
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null},
+                {null, null, null, null, null, null}
             },
             new String [] {
-                "Title 1", "Title 2", "Title 3", "Title 4"
+                "Sku", "Nombre", "Precio", "Stock", "Tipo", "InfoEspecifica"
             }
         ));
         jScrollPane1.setViewportView(jtListar);
